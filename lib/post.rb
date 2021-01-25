9c61e853ad953e35e1df8a411200112f065ee869
SuperPox <calebkohl@gmail.com>
2021-01-25 14:55:03 -0700

    commit tree 464d3e594e47a21e692527dc83d651acd98737eb

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post
+    attr_accessor :author, :title
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def title
+        @title
+    end
+
+    def author
+        @author
+    end
+
+    def author_name
+        if  @author == nil
+            return nil
+        else
+            @author.name
+        end    
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+    attr_accessor :artist, :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def name
+        @name
+    end
+
+    def artist_name
+        if  @artist == nil
+            return nil
+        else
+            @artist.name
+        end    
+    end
+end
+
